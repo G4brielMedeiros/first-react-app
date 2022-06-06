c1b62e9255d296fffdfbc85adff89cb610765035
Gabriel Medeiros <gabriel.medeiros@liferay.com>
2022-06-06 14:59:11 -0300

    set up App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { content } from "./content";
+import "./App.css";
+import Banner from "./components/Banner";
+import TravelCard from "./components/TravelCard";
 
 function App() {
+  const cards = content.map((travel) => {
+    return <TravelCard key={travel.id} travel={travel} />;
+  });
+
   return (
     <div className="App">
-      
+      <Banner text={"My Travel Journal"} />
+      <main>{cards}</main>
+      <Banner text={"The End"} />
     </div>
   );
 }
